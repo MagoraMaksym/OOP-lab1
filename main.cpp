a142793098600d6f978507a0fa0d7d15a5ba7f8f
MagoraMaksym <maksym.mahora@lnu.edu.ua>
2026-02-23 19:35:11 +0200

    Refactor cost file reading and processing logic

--- main.cpp
+++ main.cpp
@@ -1,52 +1,50 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
-#include "string"
+#include <string>
+#include <sstream>
 #include "cost.h"
 
 using namespace std;
 
 int main() {
-
-    ifstream pfile;
-    pfile.open("cost.txt");
-    char str[100];
-
-    struct Cost summ;
-    summ.hrn = 0;
-    summ.kop = 0;
-
-    while (pfile.getline(str, 100)) {
-        cout << str << "\n";
-
-        string tokens[3];
-
-        char* next_token1 = NULL;
-        char* token = strtok_r(str, " ", &next_token1);
-        int i = 0;
-
-        while (token != NULL) {
-            tokens[i] = token;
-            token = strtok_r(NULL, " ", &next_token1);
-            i++;
-        }
-
-        if (i < 3) {
-            cout << "Invalid input format: " << str << endl;
-            continue;
-        }
-
-        struct Cost a;
-        a.hrn = stoi(tokens[0]);
-        a.kop = stoi(tokens[1]);
-        int col = stoi(tokens[2]);
-        summ = sum(mult(a, col), summ);
+    ifstream pfile("cost.txt");
+    if (!pfile.is_open()) {
+        cout << "Помилка: файл не знайдено!" << endl;
+        return 1;
     }
 
-    cout << "Total cost: ";
-    to_str(summ);
-    cout << "Rounded cost: ";
-    to_str(c_round(summ));
-    pfile.close();
+    Cost total_sum = {0, 0};
+    string line;
 
+    while (getline(pfile, line)) {
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        string product_name;
+        
+        getline(ss, product_name, ',');
+
+        int h, k, q;
+        string temp;
+
+        ss >> h >> temp >> k >> temp; 
+        
+        if (ss.peek() == ',' || ss.peek() == ' ') ss.ignore();
+        
+        ss >> q >> temp; 
+
+        cout << "" << product_name << " Сума: " << h << "грн " << k << "коп x " << q << endl;
+
+        total_sum.add_item(h, k, q);
+    }
+
+    cout << "Загальна сума: ";
+    total_sum.print();
+
+    total_sum.round_to_10();
+    cout << "Після заокруглення (<5->0, >=5->10): ";
+    total_sum.print();
+
+    pfile.close();
+    return 0;
 }
